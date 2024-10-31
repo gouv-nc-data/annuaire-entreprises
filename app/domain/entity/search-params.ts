98965d03b61700f0bbaeeaccbf1c771083d9dd32
qdanneville <quentin@esperluette.io>
2024-10-31 15:47:13 +1100

    chore: pagination de résultat ajoutée

--- app/domain/entity/search-params.ts
+++ app/domain/entity/search-params.ts
@@ -3,16 +3,17 @@ export class SearchParams {
     q: string | null = null;
     ville: string[] | null = null;
     codePostal: string[] | null = null;
+    page: string | null = "1"
 
     query: string = ''
 
     constructor(params: URLSearchParams) {
 
-        console.log('params', params)
+        this.q = params.get('terme') ?? '';
+        this.page = params.get('page') ?? '1';
+        const ville = params.get('ville')
+        const codePostal = params.get('code_postal')
 
-        this.q = params.get("terme") ?? '';
-        const ville = params.get("ville")
-        const codePostal = params.get("code_postal")
 
         if (ville && ville.length > 0) {
             this.ville = ville.split(',')
@@ -27,13 +28,14 @@ export class SearchParams {
 
     buildQuery() {
 
-        console.log('this.ville', this.ville)
-        console.log('this.codePostal', this.codePostal)
-
         const searchParams = new URLSearchParams()
 
         if (this.q && this.q.length > 0) {
             searchParams.set('q', this.q)
+        }
+
+        if (this.page) {
+            searchParams.set('page', this.page)
         }
 
         if (this.ville && this.ville.length > 0) {
@@ -43,8 +45,6 @@ export class SearchParams {
         if (this.codePostal && this.codePostal.length > 0) {
             searchParams.set('code_postal', this.codePostal.join())
         }
-
-        console.log('build query : ', searchParams)
 
         this.query = searchParams.toString()
     }
---
+++ app/presentation/search/components/search-pagination.tsx
@@ -0,0 +1,69 @@
+import { useSearchParams } from "@remix-run/react";
+import { SearchResults } from "~/domain/entity/search-results";
+
+import { ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from "lucide-react";
+
+import {
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+} from "~/presentation/ui/pagination"
+
+
+export default function SearchPagination({ searchResults }: { searchResults: SearchResults }) {
+
+    const [searchParams] = useSearchParams();
+    const currentPage = parseInt(searchParams.get('page') ?? "1")
+    const totalPages = searchResults.total_pages
+
+    function createPaginationLink(toPage: number) {
+        const newSearchParams = new URLSearchParams(searchParams.toString())
+        newSearchParams.set('page', JSON.stringify(toPage))
+
+        return `rechercher?${newSearchParams.toString()}`
+    }
+
+    return (
+        <PaginationContent className="flex items w-full items-center justify-center">
+            <PaginationItem>
+                <PaginationLink href={createPaginationLink(1)}><ChevronsLeft /></PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href={createPaginationLink(currentPage - 1 > 0 ? currentPage - 1 : 1)}><ChevronLeft /></PaginationLink>
+            </PaginationItem>
+            {
+                Array.from(Array(totalPages).keys()).slice(currentPage - 5 > 0 ? currentPage - 5 : 0, currentPage - 1).map((item) => (
+                    <PaginationItem key={item + 1}>
+                        <PaginationLink
+                            href={createPaginationLink(item + 1)}>
+                            {item + 1}
+                        </PaginationLink>
+                    </PaginationItem>
+                ))
+            }
+            <PaginationItem key={currentPage}>
+                <PaginationLink
+                    isActive={true}
+                    href={createPaginationLink(currentPage)}>
+                    {currentPage}
+                </PaginationLink>
+            </PaginationItem>
+            {
+                Array.from(Array(totalPages).keys()).slice(currentPage + 1, currentPage + 5 < totalPages ? currentPage + 5 : totalPages).map((item) => (
+                    <PaginationItem key={item + 1}>
+                        <PaginationLink
+                            href={createPaginationLink(item + 1)}>
+                            {item + 1}
+                        </PaginationLink>
+                    </PaginationItem>
+                ))
+            }
+            <PaginationItem>
+                <PaginationLink href={createPaginationLink(currentPage + 1 < totalPages ? currentPage + 1 : totalPages)}><ChevronRight /></PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+                <PaginationLink href={createPaginationLink(totalPages)}><ChevronsRight /></PaginationLink>
+            </PaginationItem>
+        </PaginationContent>
+    )
+}
