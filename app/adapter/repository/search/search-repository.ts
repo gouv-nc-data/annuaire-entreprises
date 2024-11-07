05c61df890696ccac64002f436efcd13673acf3f
qdanneville <quentin@esperluette.io>
2024-11-07 17:27:08 +1100

    chore: Url api recherche fixée - variable environnement utilisée

--- app/adapter/repository/search/search-repository.ts
+++ app/adapter/repository/search/search-repository.ts
@@ -4,7 +4,7 @@ import { ISearchRepository } from '~/domain/ports/search-repository';
 export function SearchRepository(): ISearchRepository {
 
     async function getSearchResults(queryString: String): Promise<SearchResults> {
-        const res = await fetch(`https://annuaire-entreprises.qualif.gnc/api/v1/recherche?${queryString}`)
+        const res = await fetch(`${process.env.SEARCH_API_URL}?${queryString}`)
         return res.json()
     }
 
