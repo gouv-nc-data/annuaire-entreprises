c3205162d19d4a6fd0df6949ea1426514f786497
qdanneville <quentin@esperluette.io>
2024-12-05 11:10:22 +1100

    chore: function de formattage de date pour une lecture humaine

---
+++ app/utils/format-date.ts
@@ -0,0 +1,6 @@
+export function formatDate(dateString: string) {
+    const date = new Date(dateString);
+
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('fr-FR', options)
+}
