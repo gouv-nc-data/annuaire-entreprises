adf6e2946b81c7157e737b4f5f5813650f50df72
qdanneville <quentin@esperluette.io>
2024-12-05 16:52:02 +1100

    chore: ajout de la page de résultat vide

--- app/presentation/search/index.tsx
+++ app/presentation/search/index.tsx
@@ -3,6 +3,7 @@ import { getSearchResultsLoader } from "~/application/search";
 import { SearchResultsList } from "./results/search-results-list"
 import SearchResultsGuide from "./results/search-results-guide";
 import SearchFilters from "./search-filters/search-filters";
+import SearchResultsEmpty from "./results/search-results-empty";
 
 export default function SearchPage() {
     const searchResults = useLoaderData<typeof getSearchResultsLoader>();
@@ -12,9 +13,12 @@ export default function SearchPage() {
             <SearchFilters />
             <div className="py-10 max-w-7xl mx-auto px-4">
                 {
-                    searchResults !== null && searchResults.results && searchResults.total_results
-                        ? <SearchResultsList searchResults={searchResults} />
-                        : <SearchResultsGuide />
+                    searchResults === null
+                        ? <SearchResultsGuide />
+                        : searchResults.results && searchResults.total_results
+                            ? <SearchResultsList searchResults={searchResults} />
+                            : <SearchResultsEmpty />
+
                 }
             </div>
         </div>
