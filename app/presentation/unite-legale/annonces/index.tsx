2d415433fd5149ee6bdc3ec06411f5adfd355633
qdanneville <quentin@esperluette.io>
2024-12-19 09:49:00 +1100

    chore: page d'annonces d'une entreprise ajoutée

---
+++ app/presentation/unite-legale/annonces/index.tsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "@remix-run/react";
+import { getUniteLegaleLoader } from "~/application/unite-legale/get-unite-legale-by-rid";
+import { useEffect } from "react";
+import { setUniteLegaleHistoryToLocalStorage } from "~/application/unite-legale/unite-legale-history-store";
+import UniteLegaleHeader from "../unite-legale-header";
+import UniteLegaleNavigation from "../unite-legale-navigation";
+
+export default function UniteLegaleAnnoncesPage() {
+
+    const uniteLegale = useLoaderData<typeof getUniteLegaleLoader>();
+
+    useEffect(() => {
+        setUniteLegaleHistoryToLocalStorage(uniteLegale)
+    }, [])
+
+    return (
+        <div className="flex flex-col">
+            <div className="w-full max-w-7xl mx-auto px-4 py-10 flex flex-col gap-10">
+                <UniteLegaleHeader uniteLegale={uniteLegale} showShareButton />
+                <UniteLegaleNavigation rid={uniteLegale.rid} />
+            </div>
+        </div>
+    )
+}
+
---
+++ app/routes/annonces.$rid/route.tsx
@@ -0,0 +1,4 @@
+import UniteLegaleAnnoncesPage from '~/presentation/unite-legale/annonces'
+export { getUniteLegaleLoader as loader } from '~/application/unite-legale/get-unite-legale-by-rid'
+
+export default UniteLegaleAnnoncesPage
