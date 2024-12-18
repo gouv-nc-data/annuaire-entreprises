af22b3ab6495029f84dfda4026b1c29cdefd9b80
qdanneville <quentin@esperluette.io>
2024-12-19 10:32:12 +1100

    chore: entité annonce ajoutée

---
+++ app/domain/entity/annonce.ts
@@ -0,0 +1,48 @@
+export interface IAnnonce {
+    publication: string | null
+    details: string | null
+    link: string | null
+}
+
+export const fakeAnnonces: IAnnonce[] = [
+    {
+        publication: "11/03/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "12/03/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "14/03/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "15/03/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "11/08/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "12/08/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "13/08/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+    {
+        publication: "14/08/2024",
+        details: "Inscription à INFOGREFFE",
+        link: '/'
+    },
+]
