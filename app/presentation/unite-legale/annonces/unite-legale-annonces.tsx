4aac972e47a2e04c3b100a32d422d6ec4d4bf849
qdanneville <quentin@esperluette.io>
2025-01-08 15:30:33 +1100

    chore: lien vers infogreffe corrigé

--- app/presentation/unite-legale/annonces/unite-legale-annonces.tsx
+++ app/presentation/unite-legale/annonces/unite-legale-annonces.tsx
@@ -16,14 +16,14 @@ export default function UniteLegaleAnnonces({ uniteLegale }: { uniteLegale: IUni
     return (
         <section className='flex flex-col gap-6 p-6 bg-white rounded-xl shadow-sm border border-input'>
             <header className='flex w-full justify-between items-start gap-6'>
-                <h2 className='bg-blue-100 ring-2 ring-blue-200 border-1 shadow-md  text-blue-dinum  font-medium text-xl rounded-lg px-2'>Dirigeant(s)</h2>
+                <h2 className='bg-blue-100 ring-2 ring-blue-200 border-1 shadow-md  text-blue-dinum  font-medium text-xl rounded-lg px-2'>Annonce(s)</h2>
                 <Link to="https://www.isee.nc/" target='_blank'>
                     <img src={IseeLogo} className='w-20' />
                 </Link>
             </header>
             <div className='flex flex-col'>
                 <span className='text-zinc-900 font-light text-md'>Cette entreprise possède {fakeAnnonces.length} {fakeAnnonces.length > 1 ? 'annonces publiées' : 'annonce publiée'} enregisté au <strong className='font-medium'>Registre du commerce et des sociétés (RCS)</strong> tenu par l'<a className='link neutral' href='' target='_blank'>INFOGREFFE</a></span>
-                <span className='text-zinc-900 font-light text-md'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/digit/${uniteLegale.rid}/`} target='_blank'>page de l'entreprise</a> sur le site de l'INFOGREFFE</span>
+                <span className='text-zinc-900 font-light text-md'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/${uniteLegale.designation?.toLocaleLowerCase()}/${uniteLegale.rid}/`} target='_blank'>page de l'entreprise</a> sur le site de l'INFOGREFFE</span>
             </div>
             <div className='flex flex-col md:gap-0 gap-4'>
                 <div className='flex flex-col gap-6 w-full'>
