2723c30185ff7f68fa3c57930aece8f75e8f6be2
qdanneville <quentin@esperluette.io>
2025-01-15 16:49:26 +1100

    chore: Modification mineure de textes et de logos

--- app/presentation/footer/index.tsx
+++ app/presentation/footer/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@remix-run/react";
-import Logo from "../header/Logo";
 import { ExternalLink } from "lucide-react";
+import LogoHorizontalPng from '/logo-horizontal.png'
+
 
 export default function Footer() {
     return (
@@ -18,8 +19,35 @@ export default function Footer() {
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/administration" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
-                                    Administration
+                                <Link to="/partenaires" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
+                                    Partenaires
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-10 col-span-4 text-white">
+                        <ul className="flex flex-col gap-4">
+                            <li>
+                                <p className="font-normal text-sm">Autres sites</p>
+                            </li>
+                            <li>
+                                <Link to="https://guichet-entreprises.nc/" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
+                                    Guichet-entreprises.nc
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://data.gouv.nc/pages/accueil/" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
+                                    Data.gouv.nc
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.infogreffe.nc/" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
+                                    Infogreffe NC
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://avisridet.isee.nc/" className="font-light text-sm flex items-center gap-2 hover:underline text-slate-300">
+                                    Avis RIDET
                                 </Link>
                             </li>
                         </ul>
@@ -30,18 +58,17 @@ export default function Footer() {
                 <div className="max-w-7xl mx-auto px-4">
                     <div className="w-full flex-col lg:flex-row flex items-start lg:items-center justify-between py-10">
                         <Link to="/" className='lg:w-1/2'>
-                            <Logo />
+                            <img src={LogoHorizontalPng} alt="Logo de la DINUM" className='w-[150px] md:w-[200px]' />
                         </Link>
                         <div className="lg:w-1/2 flex flex-col gap-4">
                             <p className="text-white font-light text-md">
-                                Ce site permet de retrouver toutes les données publiques détenues par l’administration sur une entreprise, une association ou une administration et en particulier les données contenues dans un extrait KBIS ou de l’extrait D1.
-
-                                Il est opéré par la Direction Interministérielle du Numérique et la Direction Générale des Entreprises.
+                                Ce site permet de retrouver toutes les données publiques détenues par l’administration sur une entreprise, une association ou une administration et en particulier les données contenues dans un extrait KBIS ou un avis de situation au RIDET.
+                                Il est opéré par la Direction du Numérique et de la Modernisation du gouvernement de la Nouvelle-Calédonie.
                             </p>
                             <ul className="flex items-center text-white">
                                 <li>
-                                    <Link to="https://gouv.nc/" className="flex items-center gap-2 hover:underline">
-                                        gouv.nc
+                                    <Link to="https://numerique.gouv.nc/" className="flex items-center gap-2 hover:underline">
+                                        Numerique.gouv.nc/
                                         <ExternalLink className='w-4 h-4' />
                                     </Link>
                                 </li>
--- app/presentation/header/Logo.tsx
+++ app/presentation/header/Logo.tsx
@@ -1,9 +1,9 @@
-import LogoSvg from '/logo.svg'
+import LogoPng from '/logo.png'
 
 export default function Logo() {
     return (
         <div className="flex items-center gap-2">
-            <img src={LogoSvg} alt="Logo de la DINUM" className='w-[150px] md:w-[200px]'/>
+            <img src={LogoPng} alt="Logo de la DINUM" className='w-[150px] md:w-[200px]'/>
         </div>
     )
 }
