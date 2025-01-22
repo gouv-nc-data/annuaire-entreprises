c5c11484dcb1ed2affb37bd880c80f04e383570d
qdanneville <quentin@esperluette.io>
2025-01-22 14:59:34 +1100

    chore: UI erreur fixée

--- app/presentation/root-error-boundary.tsx
+++ app/presentation/root-error-boundary.tsx
@@ -23,7 +23,7 @@ export function RootErrorBoundary() {
                             </div>
                             {error.data
                                 ? <h2 className="text-xl text-primary flex items-start gap-2"><CornerDownRight className="w-7 h-7 -mt-[2px]" /> {error.data}</h2>
-                                : <h2 className="text-xl text-primary"><CornerDownRight /> Quelque chose s'est mal passé</h2>
+                                : <h2 className="text-xl text-primary flex items-start gap-2"><CornerDownRight /> Quelque chose s'est mal passé</h2>
                             }
                         </div>
 
@@ -47,7 +47,7 @@ export function RootErrorBoundary() {
                         <div className="flex flex-col gap-0 w-full">
                             <h1 className="text-4xl text-blue-dinum font-medium flex items-center gap-2 text-red-500"><CircleX strokeWidth={2.5} className="w-8 h-8 text-red-500" />Oops, une erreur s'est produite</h1>
                         </div>
-                        <h2 className="text-xl text-primary"><CornerDownRight /> Quelque chose s'est mal passé</h2>
+                        <h2 className="text-xl text-primary flex items-start gap-2"><CornerDownRight /> Quelque chose s'est mal passé</h2>
                     </div>
 
                     <div className="flex flex-col items-start gap-2 bg-white w-full p-6 rounded-2xl shadow-sm">
