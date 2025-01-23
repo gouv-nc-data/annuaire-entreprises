79c80c6d3d266155d6a444d389d072ba3c9518d6
qdanneville <quentin@esperluette.io>
2025-01-23 14:09:32 +1100

    chore: format python string to json fixé

--- app/utils/format-string-json.ts
+++ app/utils/format-string-json.ts
@@ -1,12 +1,24 @@
 export function formatPythonStringToJson(string: string) {
 
-    // Python to js object
-    let correctedString = string
-        .replace(/'/g, '"') // Replace single quotes with double quotes
-        .replace(/True/g, 'true') // Replace True with true
-        .replace(/None/g, 'null') // Replace None with null
-        .replace(/}\s*{/g, '},{') // Add commas between objects
-        .replace(/datetime\.datetime\(([^)]+)\)/g, 'null')
+    try {
 
-    return JSON.parse(correctedString)
+        // Python to js object
+        let correctedString = string
+            .replace(/'/g, '"') // Replace single quotes with double quotes
+            .replace(/True/g, 'true') // Replace True with true
+            .replace(/False/g, 'false') // Replace False with false
+            .replace(/None/g, 'null') // Replace None with null
+            .replace(/}\s*{/g, '},{') // Add commas between objects
+            .replace(/datetime\.datetime\(([^)]+)\)/g, 'null')
+            .replace("Président du conseil d\"administration", "Président du conseil d'administration")
+
+
+        return JSON.parse(correctedString)
+
+    }
+    catch (err) {
+        console.log('err', err)
+    }
+
+    return null
 }
