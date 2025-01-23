6d9c41f6009287a93a3c444dc9108e864f038fe1
qdanneville <quentin@esperluette.io>
2025-01-23 14:05:26 +1100

    chore: Affichage du nombre de resultat max modifié

--- app/presentation/search/results/search-results-list.tsx
+++ app/presentation/search/results/search-results-list.tsx
@@ -6,13 +6,15 @@ export function SearchResultsList({ searchResults }: { searchResults: SearchResu
 
     const currentPage = searchResults.page
 
-    console.log('search results : ', searchResults)
-
     return (
         <div className="flex flex-col gap-4">
             <div className="flex items-center gap-1 text-primary text-sm text-zinc-600 font-normal">
                 {currentPage > 1 && <span>page {currentPage} de</span>}
-                <span>{`${searchResults.total_results} ${searchResults.total_results > 1 ? 'résultats trouvés.' : 'résultat trouvé.'}`}</span>
+                {
+                    searchResults.total_results === 10000
+                        ? <span>+ de 9999 résultats trouvés.</span>
+                        : <span>{`${searchResults.total_results} ${searchResults.total_results > 1 ? 'résultats trouvés.' : 'résultat trouvé.'}`}</span>
+                }
             </div>
             <ul className="flex flex-col gap-10">
                 {
