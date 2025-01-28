1fbdc33eb89b6f78e374d5c8bfbb394beb12fa1f
qdanneville <quentin@esperluette.io>
2025-01-28 14:00:45 +1100

    chore: logos des partenaires ajouté au footer

--- app/presentation/footer/index.tsx
+++ app/presentation/footer/index.tsx
@@ -1,11 +1,47 @@
 import { Link } from "@remix-run/react";
 import { ExternalLink } from "lucide-react";
+
 import LogoHorizontalPng from '/logo-horizontal.png'
+import LogoIsee from '/logo_isee.jpg'
+import LogoGnc from '/logo_gnc.jpeg'
+import LogoCci from '/logo_cci.jpg'
+import LogoCma from '/logo_cma.png'
+import LogoCafat from '/logo_cafat.jpg'
+import LogoCapNc from '/logo_cap-nc.png'
 
 
 export default function Footer() {
     return (
         <footer className="flex flex-col">
+            <div className="bg-white border-t-1">
+                <div className="max-w-7xl w-full mx-auto px-4 py-20 gap-20 flex flex-col">
+                    <div className="flex items-center justify-center w-full">
+                        <Link to="/partenaires" className="text-xl font-light flex items-center gap-2 hover:underline text-slate-900">
+                            Nos partenaires
+                        </Link>
+                    </div>
+                    <ul className="flex flex-col sm:grid grid-cols-12 gap-4">
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoIsee} alt="Logo ISEE" className='block w-[120px] md:w-[100px]' />
+                        </li>
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoCma} alt="Logo CMA" className='w-[120px] md:w-[100px]' />
+                        </li>
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoCapNc} alt="Logo CAPNC" className='w-[120px] md:w-[100px]' />
+                        </li>
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoCci} alt="Logo CCI" className='w-[120px] md:w-[100px]' />
+                        </li>
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoCafat} alt="Logo CAFAT" className='w-[120px] md:w-[100px]' />
+                        </li>
+                        <li className="flex items-center w-full justify-center col-span-2">
+                            <img src={LogoGnc} alt="Logo GNC" className='w-[120px] md:w-[100px]' />
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <div className="bg-primary-200">
                 <div className="max-w-7xl w-full mx-auto px-4 flex flex-col sm:grid grid-cols-12 py-20 gap-20">
                     <div className="flex flex-col gap-10 col-span-4 text-white">
