4863d5bd6570754d29748d34c715b1cc4fb4ae02
qdanneville <quentin@esperluette.io>
2025-02-06 16:19:15 +1100

    chore: accessibilité fix

--- app/presentation/unite-legale/dirigeants/unite-legale-dirigeants.tsx
+++ app/presentation/unite-legale/dirigeants/unite-legale-dirigeants.tsx
@@ -3,7 +3,9 @@ import { IUniteLegale } from '~/domain/entity/unite-legale'
 
 import UniteLegaleDirigeantsListHeader from './unite-legale-dirigeants-list-header'
 
+// eslint-disable-next-line import/no-unresolved
 import IseeLogo from '/isee-logo-white.png'
+// eslint-disable-next-line import/no-unresolved
 import InfogreffeLogo from '/infogreffe.svg'
 import { Button } from '../../ui/button'
 import { InfoIcon, Users } from 'lucide-react'
@@ -18,11 +20,11 @@ export default function UniteLegaleDirigeants({ uniteLegale }: { uniteLegale: IU
             <header className='flex w-full justify-between items-start gap-6'>
                 <h2 className='bg-blue-100 ring-2 ring-blue-200 border-1 shadow-md  text-blue-dinum  font-medium text-xl rounded-lg px-2'>Dirigeant(s)</h2>
                 <div className='flex items-center gap-6'>
-                    <Link to="https://www.infogreffe.nc/" target='_blank'>
-                        <img src={InfogreffeLogo} className='w-20' />
+                    <Link to="https://www.infogreffe.nc/" target='_blank' rel="noreferrer">
+                        <img src={InfogreffeLogo} className='w-20' alt='Infogreffe Logo'/>
                     </Link>
-                    <Link to="https://www.isee.nc/" target='_blank'>
-                        <img src={IseeLogo} className='w-20' />
+                    <Link to="https://www.isee.nc/" target='_blank' rel="noreferrer">
+                        <img src={IseeLogo} className='w-20' alt='Isee Logo'/>
                     </Link>
                 </div>
             </header>
@@ -30,8 +32,8 @@ export default function UniteLegaleDirigeants({ uniteLegale }: { uniteLegale: IU
                 uniteLegale.dirigeants && uniteLegale.dirigeants.length > 0 ?
                     <div className='flex flex-col gap-6'>
                         <div className='flex flex-col'>
-                            <span className='text-common'>Cette entreprise possède {uniteLegale.dirigeants.length} {uniteLegale.dirigeants.length > 1 ? 'dirigeants' : 'dirigeant'}  enregisté au <strong className='font-medium'>Registre du commerce et des sociétés (RCS)</strong> tenu par l'<a className='link neutral' href='https://www.infogreffe.nc/' target='_blank'>INFOGREFFE</a></span>
-                            <span className='text-common'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/${uniteLegale.rid}/`} target='_blank'>page de l'entreprise</a> sur le site de l'INFOGREFFE</span>
+                            <span className='text-common'>Cette entreprise possède {uniteLegale.dirigeants.length} {uniteLegale.dirigeants.length > 1 ? 'dirigeants' : 'dirigeant'}  enregisté au <strong className='font-medium'>Registre du commerce et des sociétés (RCS)</strong> tenu par l&apos;<a className='link neutral' href='https://www.infogreffe.nc/' target='_blank' rel="noreferrer">INFOGREFFE</a></span>
+                            <span className='text-common'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/${uniteLegale.rid}/`} target='_blank' rel="noreferrer">page de l&apos;entreprise</a> sur le site de l&apos;INFOGREFFE</span>
                         </div>
                         <div className='flex flex-col md:gap-0 gap-4'>
                             <div className='flex flex-col gap-6 w-full'>
@@ -47,8 +49,8 @@ export default function UniteLegaleDirigeants({ uniteLegale }: { uniteLegale: IU
                     :
                     <div className='flex flex-col gap-6'>
                         <div className='flex flex-col'>
-                            <span className='text-common flex items-center gap-2'><Users className='text-red-500' /> Aucune information concernant les dirigeants de cette entreprise n'est disponible</span>
-                            <span className='text-common'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/${uniteLegale.rid}/`} target='_blank'>page de l'entreprise</a> sur le site de l'INFOGREFFE</span>
+                            <span className='text-common flex items-center gap-2'><Users className='text-red-500' /> Aucune information concernant les dirigeants de cette entreprise n&apos;est disponible</span>
+                            <span className='text-common'>Pour en savoir plus, vous pouvez consulter la <a className='link neutral' href={`https://www.infogreffe.nc/entreprise/${uniteLegale.rid}/`} target='_blank' rel="noreferrer">page de l&apos;entreprise</a> sur le site de l&apos;INFOGREFFE</span>
                         </div>
                     </div>
             }
@@ -56,7 +58,7 @@ export default function UniteLegaleDirigeants({ uniteLegale }: { uniteLegale: IU
             <div className='flex flex-col md:flex-row items-start md:items-center justify-end gap-4'>
                 <div className='flex items-center gap-4'>
                     <span className='text-xs font-light text-slate-500'>Mise à jour le 05/12/2024</span>
-                    <Link to="https://www.infogreffe.nc/" target='_blank'>
+                    <Link to="https://www.infogreffe.nc/" target='_blank' rel="noreferrer">
                         <Button variant={"outline"}>
                             <InfoIcon className="w-3 h-3" />
                             Sources: INFOGREFFE
