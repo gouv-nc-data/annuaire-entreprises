8f847dea6a3c445749f95032668496307a76c4ab
houysy <90165900+houysy@users.noreply.github.com>
2025-04-29 09:02:31 +1100

    Update index.tsx
    Modif texte page DONNEES PERSONNELLES

--- app/presentation/pages/donnees-personnelles/index.tsx
+++ app/presentation/pages/donnees-personnelles/index.tsx
@@ -1,104 +1,56 @@
 export default function DonneesPersonellesPage() {
     return (
         <div>
-            <div className='max-w-7xl mx-auto px-4 w-full flex justify-between items-center py-20 gap-8'>
-                <article className="max-w-4xl flex flex-col gap-10">
-                    <div className="flex flex-col gap-4">
-                        <h1 className="title-big">À propos de l’Annuaire des Entreprises test</h1>
-                        <p className="text-common">Ce site permet de retrouver toutes les données publiques détenues par l’administration sur une entreprise, une association ou une administration. Ce site ne fait que centraliser les données. Il ne les modifie pas et ne les stocke pas. Il est opéré par une équipe de la Direction Interministérielle du Numérique.</p>
-                        <p className="text-common">Ce site ne fait que centraliser les données. Il ne les modifie pas et ne les stocke pas.</p>
-                        <p className="text-common">Il est opéré par une équipe de la <a href="/" className="link-common">Direction Interministérielle du Numérique.</a></p>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h1 className="title-medium">Que trouve-t-on dans l’Annuaire des Entreprises ?</h1>
-                        <p className="text-common">Les fiches de l’Annuaire regroupent des informations légales sur toutes les personnes morales basées en France (entreprises, associations, administrations, entrepreneurs…) à travers plusieurs onglets :</p>
-                        <ul className="list-disc ps-6 flex flex-col gap-2">
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Résumé :</strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Justificatif d’immatriculation :</strong> permettant d’obtenir le document pour prouver l’existence d’une entreprise ou d’une association (équivalent des extraits KBIS et D1)
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    Informations sur les <strong className="font-semibold">dirigeants</strong> (d’entreprises) ou élus (collectivités)
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Annonces légales :</strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Données financières des entreprises : </strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Annonces légales :</strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Labels et certificats :</strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <strong className="font-semibold">Conventions collectives :</strong> les informations générales (adresse, SIRET, SIREN, code NAF/ APE, numéro de TVA, RNA pour les associations…), les informations sur le siège social ainsi que la liste des établissements.
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h1 className="title-medium">D’où viennent les informations affichées par l’Annuaire des Entreprises ?</h1>
-                        <p className="text-common">Toutes les informations affichées sur ce site sont des informations publiques, accessibles librement et gratuitement. On appelle cela des données ouvertes ou open data.</p>
-                        <p className="text-common">Ces bases de données sont récupérées grâce aux téléservices développés par les <a href="/" className="link-common">administrations partenaires</a> :</p>
-                        <ul className="list-disc ps-6 flex flex-col gap-2">
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p className="text-common">
-                                    <a href="/" className="link-common">administrations partenaires</a>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </article>
-            </div>
-        </div>
+      <div className='max-w-7xl mx-auto px-4 w-full flex justify-between items-center py-20 gap-8'>
+        <article className="max-w-4xl flex flex-col gap-10">
+
+          <div className="flex flex-col gap-4">
+            <h1 className="title-big">Protection des données personnelles</h1>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="title-medium">Données collectées</h1>
+            <p className="text-common">
+              Ce site ne collecte aucune donnée personnelle directement auprès de ses utilisateurs.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="title-medium">Données affichées</h1>
+            <p className="text-common">
+              Les informations affichées concernent les entreprises (raison sociale, adresse, statut juridique, numéro RIDET, numéro RCS, etc.). 
+              Elles proviennent de bases de données publiques administrées par :
+            </p>
+            <ul className="list-disc ps-6 flex flex-col gap-2">
+              <li>
+                <p className="text-common">
+                  l'<strong className="font-semibold">ISEE</strong> pour le RIDET ;
+                </p>
+              </li>
+              <li>
+                <p className="text-common">
+                  la <strong className="font-semibold">DAE</strong> pour le RCS.
+                </p>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="title-medium">Droits des personnes</h1>
+            <p className="text-common">
+              Conformément à la réglementation applicable en Nouvelle-Calédonie, toute personne peut demander la rectification des données la concernant auprès de l'organisme source (ISEE ou DAE).
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="title-medium">Contact</h1>
+            <p className="text-common">
+              Pour toute question relative à la protection de vos données, vous pouvez écrire à : <span className="font-semibold">[adresse email à compléter]</span>.
+            </p>
+          </div>
+
+        </article>
+      </div>
+    </div>
     )
 }
