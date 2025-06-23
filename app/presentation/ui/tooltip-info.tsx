2804f97e59c034cbcbbc4fe1ec7bd89826ea4476
qdanneville <quentin@esperluette.io>
2025-06-24 10:10:00 +1100

    chore: tooltip ui modifié

--- app/presentation/ui/tooltip-info.tsx
+++ app/presentation/ui/tooltip-info.tsx
@@ -5,7 +5,7 @@ export default function TooltipInfo({ label, content, isEtablissement }: { label
     return (
         <TooltipProvider delayDuration={0}>
             <Tooltip>
-                <TooltipTrigger className={`cursor-help inline-flex items-center gap-2 border-b-2 border-dashed ${isEtablissement ? 'border-orange-dinum' : 'border-blue-dinum'}`}>
+                <TooltipTrigger className={`cursor-help inline-flex items-center gap-1`}>
                     <span className='text-base font-normal text-slate-700 whitespace-nowrap'>{label}</span>
                     <Info className='w-5 h-5 text-white fill-blue-dinum' />
                 </TooltipTrigger>
