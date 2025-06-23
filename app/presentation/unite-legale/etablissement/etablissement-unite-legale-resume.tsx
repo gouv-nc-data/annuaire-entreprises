5496ce13ec2371793ef4d7cb680285b6ee00039f
qdanneville <quentin@esperluette.io>
2025-06-23 15:00:17 +1100

    chore: etablissement entreprise encart modifié

--- app/presentation/unite-legale/etablissement/etablissement-unite-legale-resume.tsx
+++ app/presentation/unite-legale/etablissement/etablissement-unite-legale-resume.tsx
@@ -12,17 +12,17 @@ export default function EtablissementUniteLegaleResume({ uniteLegale, etablissem
             <div className="flex flex-col w-full pt-4 gap-2">
                 <p className="flex items-center gap-2 text-slate-700 font-light text-base">
                     <CornerDownRightIcon strokeWidth={1.4} className="w-5 h-5 text-slate-600" />
-                    Cet établissement est lié à l&apos;entreprise de :
+                    Cet établissement est lié à l&apos;entreprise :
                 </p>
                 <div className="flex flex-col items-start bg-white p-4 rounded-xl shadow-sm border border-input w-full gap-4">
-                    <div className="flex flex-row items-center gap-2">
+                    <div className="flex flex-col items-start gap-2">
                         <Link to={`/entreprise/${uniteLegale.rid}`} className="inline-flex text-blue-dinum border-b border-blue-dinum grow shrink-0">
                             {uniteLegale.nom_complet}
                         </Link>
                         <CommonRidetStatus uniteLegale={uniteLegale} />
                     </div>
                     <hr className="w-full" />
-                    <UniteLegaleNavigation rid={uniteLegale.rid} isEtablissement />
+                    <UniteLegaleNavigation rid={uniteLegale.rid} />
                 </div>
             </div>
         </div>
--- app/presentation/unite-legale/unite-legale-navigation.tsx
+++ app/presentation/unite-legale/unite-legale-navigation.tsx
@@ -2,16 +2,16 @@ import { Link, useLocation } from "@remix-run/react";
 import { IUniteLegale } from "~/domain/entity/unite-legale";
 import { BookText, UsersRound, Newspaper } from "lucide-react";
 
-export default function UniteLegaleNavigation({ rid, isEtablissement }: { rid: IUniteLegale['rid'], isEtablissement?: boolean }) {
+export default function UniteLegaleNavigation({ rid }: { rid: IUniteLegale['rid'] }) {
 
     const location = useLocation()
 
     return (
-        <nav className="w-full">
-            <ul className={`${isEtablissement ? 'flex flex-col' : 'flex sm:inline-flex sm:flex-row flex-col sm:items-center bg-white p-1 rounded-xl shadow-sm border border-input gap-2'}`}>
+        <nav className="">
+            <ul className="inline-flex items-center gap-1 bg-white p-1 rounded-xl shadow-sm border border-input">
                 <li>
                     <Link
-                        className={`flex gap-2 items-center rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('entreprise') ? 'bg-blue-50  ring-blue-200 border-1 text-blue-dinum font-normal' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
+                        className={`flex gap-2 items-center rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('entreprise') ? 'bg-blue-50  ring-blue-200 border-1  text-blue-dinum  font-medium' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
                         to={`/entreprise/${rid}`}
                     >
                         <BookText strokeWidth={1.2} className={`${location.pathname.includes('entreprise') ? 'fill-blue-200' : 'fill-blue-50'} w-5 h-5 text-blue-dinum`} />
@@ -20,7 +20,7 @@ export default function UniteLegaleNavigation({ rid, isEtablissement }: { rid: I
                 </li>
                 <li>
                     <Link
-                        className={`flex gap-2 items-center  rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('dirigeants') ? 'bg-blue-50  ring-blue-200 border-1  text-blue-dinum  font-normal' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
+                        className={`flex gap-2 items-center  rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('dirigeants') ? 'bg-blue-50  ring-blue-200 border-1  text-blue-dinum  font-medium' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
                         to={`/dirigeants/${rid}`}>
                         <UsersRound strokeWidth={1.2} className={`${location.pathname.includes('dirigeants') ? 'fill-blue-200' : 'fill-blue-50'} w-5 h-5 text-blue-dinum `} />
                         <span>Dirigeants</span>
@@ -28,7 +28,7 @@ export default function UniteLegaleNavigation({ rid, isEtablissement }: { rid: I
                 </li>
                 <li>
                     <Link
-                        className={`flex gap-2 items-center  rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('annonces') ? 'bg-blue-50  ring-blue-200 border-1  text-blue-dinum  font-normal' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
+                        className={`flex gap-2 items-center  rounded-lg p-2 px-3 font-light border-1 text-sm ${location.pathname.includes('annonces') ? 'bg-blue-50  ring-blue-200 border-1  text-blue-dinum  font-medium' : 'text-slate-500 hover:bg-blue-50 hover:text-slate-600 border-transparent'}`}
                         to={`/annonces/${rid}`}>
                         <Newspaper strokeWidth={1.2} className={`${location.pathname.includes('annonces') ? 'fill-blue-200' : 'fill-blue-50'} w-5 h-5 text-blue-dinum`} />
                         <span>Annonces</span>
