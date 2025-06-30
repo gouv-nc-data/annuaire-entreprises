e3ffd8a22767f4c5d5f6ac44de367095a4e33588
qdanneville <quentin@esperluette.io>
2025-06-30 14:04:11 +1100

    chore: fond unifié pour les pages entreprises

--- app/presentation/unite-legale/dirigeants/index.tsx
+++ app/presentation/unite-legale/dirigeants/index.tsx
@@ -16,7 +16,7 @@ export default function UniteLegaleDirigeantsPage() {
     }, [])
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-zinc-50">
             <div className="w-full max-w-7xl mx-auto px-4 py-10 flex flex-col gap-10">
                 <UniteLegaleHeader uniteLegale={uniteLegale} showShareButton />
                 <UniteLegaleNavigation rid={uniteLegale.rid} />
