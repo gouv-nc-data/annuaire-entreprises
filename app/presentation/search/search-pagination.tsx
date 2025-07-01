9c320b0902618810059c9e2c9c3613b4dedec5f7
qdanneville <quentin@esperluette.io>
2025-07-01 16:00:56 +1100

    chore: l'UX de la pagination est amélioré

--- app/presentation/search/search-pagination.tsx
+++ app/presentation/search/search-pagination.tsx
@@ -24,46 +24,48 @@ export default function SearchPagination({ searchResults }: { searchResults: Sea
     }
 
     return (
-        <PaginationContent className="flex items w-full items-center justify-center max-w-xs sm:max-w-full overflow-x-auto sm:overflow-auto mt-4">
-            <PaginationItem>
-                <PaginationLink href={createPaginationLink(1)}><ChevronsLeft /></PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-                <PaginationLink href={createPaginationLink(currentPage - 1 > 0 ? currentPage - 1 : 1)}><ChevronLeft /></PaginationLink>
-            </PaginationItem>
-            {
-                Array.from(Array(totalPages).keys()).slice(currentPage - 5 > 0 ? currentPage - 5 : 0, currentPage - 1).map((item) => (
-                    <PaginationItem key={item + 1}>
-                        <PaginationLink
-                            href={createPaginationLink(item + 1)}>
-                            {item + 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                ))
-            }
-            <PaginationItem key={currentPage}>
-                <PaginationLink
-                    isActive={true}
-                    href={createPaginationLink(currentPage)}>
-                    {currentPage}
-                </PaginationLink>
-            </PaginationItem>
-            {
-                Array.from(Array(totalPages).keys()).slice(currentPage, currentPage + 5 < totalPages ? currentPage + 5 : totalPages).map((item) => (
-                    <PaginationItem key={item + 1}>
-                        <PaginationLink
-                            href={createPaginationLink(item + 1)}>
-                            {item + 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                ))
-            }
-            <PaginationItem>
-                <PaginationLink href={createPaginationLink(currentPage + 1 < totalPages ? currentPage + 1 : totalPages)}><ChevronRight /></PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-                <PaginationLink href={createPaginationLink(totalPages)}><ChevronsRight /></PaginationLink>
-            </PaginationItem>
-        </PaginationContent>
+        totalPages > 1 ? (
+            <PaginationContent className="flex items w-full items-center justify-center max-w-xs sm:max-w-full overflow-x-auto sm:overflow-auto mt-4">
+                <PaginationItem>
+                    <PaginationLink className={`${currentPage === 1 ? 'opacity-30 pointer-events-none' : ''}`} href={createPaginationLink(1)}><ChevronsLeft /></PaginationLink>
+                </PaginationItem>
+                <PaginationItem >
+                    <PaginationLink className={`${currentPage === 1 ? 'opacity-30 pointer-events-none' : ''}`} href={createPaginationLink(currentPage - 1 > 0 ? currentPage - 1 : 1)}><ChevronLeft /></PaginationLink>
+                </PaginationItem>
+                {
+                    Array.from(Array(totalPages).keys()).slice(currentPage - 5 > 0 ? currentPage - 5 : 0, currentPage - 1).map((item) => (
+                        <PaginationItem key={item + 1}>
+                            <PaginationLink
+                                href={createPaginationLink(item + 1)}>
+                                {item + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))
+                }
+                <PaginationItem key={currentPage}>
+                    <PaginationLink
+                        isActive={true}
+                        href={createPaginationLink(currentPage)}>
+                        {currentPage}
+                    </PaginationLink>
+                </PaginationItem>
+                {
+                    Array.from(Array(totalPages).keys()).slice(currentPage, currentPage + 5 < totalPages ? currentPage + 5 : totalPages).map((item) => (
+                        <PaginationItem key={item + 1}>
+                            <PaginationLink
+                                href={createPaginationLink(item + 1)}>
+                                {item + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))
+                }
+                <PaginationItem>
+                    <PaginationLink className={`${currentPage === totalPages ? 'opacity-30 pointer-events-none' : ''}`} href={createPaginationLink(currentPage + 1 < totalPages ? currentPage + 1 : totalPages)}><ChevronRight /></PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink className={`${currentPage === totalPages ? 'opacity-30 pointer-events-none' : ''}`} href={createPaginationLink(totalPages)}><ChevronsRight /></PaginationLink>
+                </PaginationItem>
+            </PaginationContent>
+        ) : <div className="h-10"></div>
     )
 }
