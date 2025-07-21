87c844d83608a253309482d94c1e46eb38a31037
qdanneville <quentin@esperluette.io>
2025-07-21 13:49:43 +1100

    chore: json > data

--- app/application/search/get-search-results.ts
+++ app/application/search/get-search-results.ts
@@ -1,4 +1,4 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { data, LoaderFunctionArgs } from "@remix-run/node";
 import { Repository } from "~/adapter/repository";
 import { SearchParams } from "~/domain/entity/search-params";
 
@@ -7,22 +7,22 @@ export async function getSearchResultsLoader({ request }: LoaderFunctionArgs) {
     let { searchParams } = new URL(request.url);
 
     if (searchParams.toString().length === 0) {
-        return json(null)
+        return data(null)
     }
 
     const query = new SearchParams(searchParams).query
     const searchResults = await Repository.search.getSearchResults(query)
 
     if (searchResults === null) {
-        throw json("Not Found", { status: 404 });
+        throw data("Not Found", { status: 404 });
     }
 
     if (searchResults.results === null) {
-        throw json("Not Found", { status: 404 });
+        throw data("Not Found", { status: 404 });
     }
 
     if (searchResults.erreur) {
-        throw json(searchResults.erreur, { status: 400 });
+        throw data(searchResults.erreur, { status: 400 });
     }
 
     return searchResults
